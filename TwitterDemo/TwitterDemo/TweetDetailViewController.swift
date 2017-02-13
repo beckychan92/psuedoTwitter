8f89232c8770b10babf5e162f64b6b706f0f1c08
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-02-13 00:48:27 -0800

    retweeting and linking on detail page works

--- TwitterDemo/TwitterDemo/TweetDetailViewController.swift
+++ TwitterDemo/TwitterDemo/TweetDetailViewController.swift
@@ -61,15 +61,71 @@ class TweetDetailViewController: UIViewController {
         }
         
     }
+    
+    func handleCount(str: String, increment: Bool) -> String{
+        let incrementor = increment ? 1 : -1
+        if let count = Int(str){
+            if count > 0{
+                return "\(count + incrementor)"
+            }else{
+                return "0"
+            }
+        }else{
+            return "0"
+        }
+    }
 
 
     @IBAction func onFavorite(_ sender: Any) {
-        
+        if !(tweet.favorited){
+            TwitterClient.sharedInstance.favorite(params: ["id": tweet.id], success: {
+                () in
+                print("like in detail controller!")
+                self.favoriteCountLabel.text = self.handleCount(str: self.favoriteCountLabel.text!, increment: true)
+                self.tweet.favorited = true
+                self.favoriteButton.setImage(#imageLiteral(resourceName: "didlike"), for: UIControlState())
+                
+            }, failure: { (error: Error) in
+                print("Error in like'tweet onFavorite': \(error.localizedDescription)")
+            })
+        }else{
+            TwitterClient.sharedInstance.unfavorite(params: ["id": tweet.id], success: {
+                () in
+                print("unlike in detail controller!")
+                self.favoriteCountLabel.text =  self.handleCount(str: self.favoriteCountLabel.text!, increment: false)
+                self.tweet.favorited = false
+                self.favoriteButton.setImage(#imageLiteral(resourceName: "like"), for: UIControlState())
+            }, failure: { (error: Error) in
+                print("Error in unlike'tweet onFavorite': \(error.localizedDescription)")
+            })
+        }
     }
     
 
     @IBAction func onRetweet(_ sender: Any) {
-        
+        if !(tweet.retweeted){
+            TwitterClient.sharedInstance.retweet(id: tweet.id, success: {
+                () in
+                print("repost!")
+                self.retweetCountLabel.text =  self.handleCount(str: self.retweetCountLabel.text!, increment: true)
+                self.tweet.retweeted = true
+                self.retweetButton.setImage(#imageLiteral(resourceName: "repost"), for: UIControlState())
+                
+            }, failure: { (error: Error) in
+                print("Error in retweet 'tweet onRetweet': \(error.localizedDescription)")
+            })
+        }else{
+            TwitterClient.sharedInstance.unretweet(id: tweet.id, success: {
+                () in
+                print("unrepost!")
+                self.retweetCountLabel.text =  self.handleCount(str: self.retweetCountLabel.text!, increment: false)
+                self.tweet.retweeted = false
+                self.retweetButton.setImage(#imageLiteral(resourceName: "notRepost"), for: UIControlState())
+            }, failure: { (error: Error) in
+                print("Error in unreet 'tweet onRetweet': \(error.localizedDescription)")
+            })
+        }
+
     }
 
     @IBAction func onReply(_ sender: Any) {
